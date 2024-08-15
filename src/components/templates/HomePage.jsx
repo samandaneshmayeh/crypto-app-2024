a7a177b8f10b02592a47f0fdd9d34ad5e8011cad
Saman Daneshmayeh <daneshmayeh.saman@gmail.com>
2024-08-15 20:42:37 +0330

    a

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react"
 import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 
 function HomePage() {
 
     const [coins, setCoins] = useState([])
     const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         const getData = async () => {
-            const res = await fetch(getCoinList());
+          setIsLoading(true);
+            const res = await fetch(getCoinList(page));
             const json = await res.json();
             setCoins(json);
             setIsLoading(false);
         }
         getData();
-    }, [])
+    }, [page])
 
   return (
     <div>
       <TableCoin coins={coins} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
     </div>
   )
 }
